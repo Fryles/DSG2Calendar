a35c78ab0de821c5a1eea5456f6af0df62e11468
fryles <mylesmarr@gmail.com>
2021-03-23 16:32:09 -0700

    Ability to add work events to google calendar

--- main.py
+++ main.py
@@ -2,9 +2,16 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import Select
-import sys,os,time
+
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+
+import sys,os,time,pytz
+from datetime import datetime
 import config as cfg
+import Service as srv
 username = cfg.kronos['user']
 password = cfg.kronos['password']
 #days out to check schedule, keep this as a const for now..
@@ -44,11 +51,37 @@ def get_dates():
 			d = driver.find_element_by_xpath('//*[@id="column-Date-Row-'+str(i)+'"]/div').text.split(' ')[1]
 			times[d] = t
 	print(times)
+	return times
 
-
+def add_to_cal(dates):
+	service = srv.getService()
+	now = datetime.now()
+	for date, time in dates.items():
+		st = datetime.strptime(time[0], '%I:%M%p')
+		et = datetime.strptime(time[1], '%I:%M%p')
+		sh,sm = st.hour, st.minute
+		eh,em = et.hour, et.minute
+		m,d = [int(z) for z in date.split('/')]
+		start = datetime(now.year, m, d, sh, sm, 0, 0).isoformat()
+		end = datetime(now.year, m, d, eh, em, 0, 0).isoformat()
+		event = {
+			'summary': 'Work',
+			'location': '231 W Esplanade Dr, Oxnard, CA 93036',
+			'description': 'Building bikes...',
+			'start': {
+				'dateTime': start,
+				'timeZone': 'America/Chicago',#this is so hacky but its fine for now
+			},
+			'end': {
+				'dateTime': end,
+				'timeZone': 'America/Chicago',#this is so hacky but its fine for now
+			}
+		}
+		event = service.events().insert(calendarId='primary', body=event).execute()
 
 
 if __name__ == "__main__":
 	driver = webdriver.Chrome(chrome_options=chrome_options, executable_path=chrome_driver)
-	get_dates()
+	dates = get_dates()
+	add_to_cal(dates)
 	#driver.close()
